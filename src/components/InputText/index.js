ba4cbe7913aeee44ad5e46d8f0b34729841b80a4
Vitor Fonseca <vitor.oliveira@talkdesk.com>
2022-07-29 15:58:42 +0100

    Inserting type on input component

--- src/components/InputText/index.js
+++ src/components/InputText/index.js
@@ -1,14 +1,14 @@
 import { Icon } from "../Icon";
 import styles from "./input-text.module.scss";
 
-const InputText = ({ placeholder, icon = undefined, className }) => {
+const InputText = ({ placeholder, icon = undefined, className, type }) => {
   return (
     <div className={`${styles["l-input-wrapper"]} ${className}`}>
       {icon && <Icon className={styles["c-icon"]} name={icon}></Icon>}
       <input
         className={styles["c-input-text"]}
-        type="text"
         placeholder={placeholder}
+        type={type}
       ></input>
     </div>
   );
