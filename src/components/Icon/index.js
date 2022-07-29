e0ae199b03a98745fa4c5a5ff56b822bc4c337c7
Vitor Fonseca <vitor.oliveira@talkdesk.com>
2022-07-29 15:57:26 +0100

    Changing alt image on icon component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,6 +1,6 @@
 const Icon = ({ name, ...props }) => {
   return (
-    <img src={`assets/icons/${name}.svg`} alt="Email icon" {...props}></img>
+    <img src={`assets/icons/${name}.svg`} alt={`${name} icon`} {...props}></img>
   );
 };
 
