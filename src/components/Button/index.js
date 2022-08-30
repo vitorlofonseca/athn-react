ace150f7015dc3473c47a872870f2f0decc503d1
Vitor Fonseca <vitor.oliveira@talkdesk.com>
2022-08-30 17:32:29 +0100

    Creating primary button style

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,14 @@ import { Icon } from "../Icon";
 import styles from "./button.module.scss";
 import { Link } from "react-router-dom";
 
-const Button = ({ label, type, className, icon, route, onClick, ...props }) => {
+const Button = ({ type, className, icon, route, onClick, ...props }) => {
   const button = (
-    <button className={`${styles["c-button"]} ${className}`} onClick={onClick}>
+    <button
+      className={`${styles["c-button"]} ${className} ${
+        type === "primary" && styles["c-button--primary"]
+      }`}
+      onClick={onClick}
+    >
       <div className={styles["c-button-content"]}>
         {icon && <Icon name={icon} className={styles["c-icon"]}></Icon>}
         {props.children}
